71e520db814974d0c558075a9d712228428c8715
Ax0my10rk <dapelly4@gmail.com>
2025-04-05 09:35:02 -0400

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -8,6 +8,7 @@ del /f /q "C:\Windows\Temp\443pmR.exe"
 del /f /q "C:\Windows\Temp\ncat.exe"
 del /f /q "C:\Windows\SysWOW64\libcrypto-3.dll"
 del /f /q "C:\Windows\SysWOW64\libssl-3.dll"
+del /f /q "C:\Windows\SysWOW64\vcruntime140.dll"
 powershell -Command "Get-MpPreference | ForEach-Object {Remove-MpPreference -ExclusionPath $_.ExclusionPath}"
 
 :: powershell -Command "Remove-MpPreference -ExclusionPath 'C:\Users\%USERNAME%\Project\Roll\Staging\MinGW'"
