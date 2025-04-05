817a11f09f4685eca23137f3e162363298a590c6
Ax0my10rk <dapelly4@gmail.com>
2025-04-05 08:21:02 -0400

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,13 +1,15 @@
-schtasks /delete /tn "ArmorKing" /f
-schtasks /delete /tn "Bob" /f
 schtasks /delete /tn "immagina" /f
 schtasks /delete /tn "puoi" /f
+schtasks /delete /tn "ArmorKing" /f
+schtasks /delete /tn "Bob" /f
 rmdir /s /q "C:\Users\%USERNAME%\Project"
+del /f /q "C:\Windows\Temp\443pR.exe"
+del /f /q "C:\Windows\Temp\443pmR.exe"
+del /f /q "C:\Windows\Temp\ncat.exe"
 del /f /q "C:\Windows\SysWOW64\libcrypto-3.dll"
 del /f /q "C:\Windows\SysWOW64\libssl-3.dll"
-del /f /q "C:\Windows\Temp\ncat.exe"
-timeout /t 5 >nul
 powershell -Command "Get-MpPreference | ForEach-Object {Remove-MpPreference -ExclusionPath $_.ExclusionPath}"
+
 :: powershell -Command "Remove-MpPreference -ExclusionPath 'C:\Users\%USERNAME%\Project\Roll\Staging\MinGW'"
 :: schtasks /query /tn "Bob"
 :: schtasks /query /tn "ArmorKing"
